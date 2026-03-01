b23f3d06d31c552d6a7f1541458f04f3f1aa5e4b
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2026-03-01 22:17:53 +0000

    refactor App component to render DogStatusCard using mapped dog data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
 // import { useState } from "react";
 import "./App.css";
 // import React from "react";
-import bernerImage from "src/images/berner.jpeg";
+import bernerImage from "./images/berner.jpeg";
 
 import DogStatusCard from "./components/DogStatusCard.tsx";
+
+const dogs = [
+  {
+    dogId: "1",
+    dogName: "Bear K",
+    dogImage: bernerImage,
+    lastFedHours: 2,
+    lastWalkMinutes: 30,
+    lastToiletHours: 1,
+    lastMedsHours: 4,
+  },
+  {
+    dogId: "2",
+    dogName: "Ada the Shark",
+    dogImage: bernerImage,
+    lastFedHours: 6,
+    lastWalkMinutes: 90,
+    lastToiletHours: 2,
+    lastMedsHours: 2,
+  },
+  {
+    dogId: "3",
+    dogName: "Bella",
+    dogImage: bernerImage,
+    lastFedHours: 1,
+    lastWalkMinutes: 45,
+    lastToiletHours: 3,
+    lastMedsHours: 1,
+  },
+];
 
 function App() {
   return (
@@ -11,22 +41,17 @@ function App() {
       <main>
         <h1>Dog Care Log</h1>
       </main>
-      <DogStatusCard
-        dogName="Bear K"
-        dogImage={bernerImage}
-        lastFedHours={2}
-        lastWalkMinutes={30}
-        lastToiletHours={1}
-        lastMedsHours={4}
-      />
-      <DogStatusCard
-        dogName="Ada the Shark"
-        dogImage={bernerImage}
-        lastFedHours={6}
-        lastWalkMinutes={90}
-        lastToiletHours={2}
-        lastMedsHours={2}
-      />
+      {dogs.map((dog, index) => (
+        <DogStatusCard
+          key={index}
+          dogName={dog.dogName}
+          dogImage={dog.dogImage}
+          lastFedHours={dog.lastFedHours}
+          lastWalkMinutes={dog.lastWalkMinutes}
+          lastToiletHours={dog.lastToiletHours}
+          lastMedsHours={dog.lastMedsHours}
+        />
+      ))}
     </>
   );
 }
