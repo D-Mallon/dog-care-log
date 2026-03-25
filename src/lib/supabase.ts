9813d65f2ec9cf3289d3c8ef03bd76d606f1bb82
D-Mallon <115174055+D-Mallon@users.noreply.github.com>
2026-03-25 22:28:55 +0000

    debugging why vercel not picking up on supabaseUrl

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
